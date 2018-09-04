70709bc3000b92a6a631be5873c17ca2deb175e1
illusioninlost <rchang9999@yahoo.com>
2018-09-04 09:15:55 -0700

    add product

---
+++ db/migrate/20180904161003_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :quantity
+      t.string :integer
+      t.integer :amount
+      t.string :farm
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_222343) do
+ActiveRecord::Schema.define(version: 2018_09_04_161003) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "quantity"
+    t.string "integer"
+    t.integer "amount"
+    t.string "farm"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
