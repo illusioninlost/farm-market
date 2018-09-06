3025005908824bc402c74f6f156d2de94157585f
illusioninlost <rchang9999@yahoo.com>
2018-09-06 08:45:41 -0700

    add productcontroller.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,13 @@
 (function(){
     'use strict';
 
+    function ProductController($scope) {
+        $scope.name = 'test'
+    }
+
+    ProductController.$inject = ['$scope']
+
     angular
-        .module('food-market', ['ui.router', 'templates', 'Devise'])
+        .module('food-market')
         .controller('ProductController', ProductController)
 }());
